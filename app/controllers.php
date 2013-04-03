ff07b2b91765eb436b327020c64ca1e64b643828
Dwi Sasongko S <ruckuus@gmail.com>
2013-04-04 01:05:23 +0800

    Get Github repository list. No auth yet..

--- app/controllers.php
+++ app/controllers.php
@@ -6,14 +6,6 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Validator\Constraints as Assert;
 
 $app->match('/', function() use ($app) {
-    /*
-    $app['session']->getFlashBag()->add('warning', 'Warning flash message');
-    $app['session']->getFlashBag()->add('info', 'Info flash message');
-    $app['session']->getFlashBag()->add('success', 'Success flash message');
-    $app['session']->getFlashBag()->add('error', 'Error flash message');
-    */
-
-    $app['session']->getFlashBag()->add('warning', 'You are not logged in');
 
     return $app['twig']->render('index.html.twig');
 })->bind('homepage');
@@ -24,33 +16,6 @@ $app->match('/about', function() use ($app) {
         'data' => $aboutus,
     ));
 })->bind('about');
-
-$app->match('/repo', function(Request $request) use ($app) {
-    $form = $app['form.factory']->createBuilder('form')
-        ->add('github_username', 'text', array('label' => 'Github Username', 'data' => $app['session']->get('_security.last_username')))
-        ->add('github_password', 'password', array('label' => 'Github Password'))
-        ->getForm()
-    ;
-
-    if ('POST' == $request->getMethod()) {
-        $form->bind($request);
-
-        if ($form->isValid()) {
-            $data = $form->getData();
-            //var_dump($data); die();
-            $user = $data['github_username'];
-            $repos = $app['github']->api('user')->repositories($user);
-            return $app['twig']->render('repos.html.twig', array(
-                'data' => $repos,
-            ));
-        }
-    }
-    
-    return $app['twig']->render('repo.html.twig', array(
-        'form' => $form->createView(),
-        'error' => $app['security.last_error']($request),
-    ));
-})->bind('repo');
 
 $app->match('/login', function(Request $request) use ($app) {
     $form = $app['form.factory']->createBuilder('form')
@@ -65,116 +30,37 @@ $app->match('/login', function(Request $request) use ($app) {
     ));
 })->bind('login');
 
-$app->match('/doctrine', function() use ($app) {
-    return $app['twig']->render(
-        'doctrine.html.twig',
-        array(
-            'posts' => $app['db']->fetchAll('SELECT * FROM post')
-        )
-    );
-})->bind('doctrine');
-
-$app->match('/form', function(Request $request) use ($app) {
-
-    $builder = $app['form.factory']->createBuilder('form');
-    $choices = array('choice a', 'choice b', 'choice c');
-
-    $form = $builder
-        ->add(
-            $builder->create('sub-form', 'form')
-                ->add('subformemail1', 'email', array(
-                    'constraints' => array(new Assert\NotBlank(), new Assert\Email()),
-                    'attr'        => array('placeholder' => 'email constraints'),
-                    'label'       => 'A custome label : ',
-                ))
-                ->add('subformtext1', 'text')
-        )
-        ->add('text1', 'text', array(
-            'constraints' => new Assert\NotBlank(),
-            'attr'        => array('placeholder' => 'not blank constraints')
-        ))
-        ->add('text2', 'text', array('attr' => array('class' => 'span1', 'placeholder' => '.span1')))
-        ->add('text3', 'text', array('attr' => array('class' => 'span2', 'placeholder' => '.span2')))
-        ->add('text4', 'text', array('attr' => array('class' => 'span3', 'placeholder' => '.span3')))
-        ->add('text5', 'text', array('attr' => array('class' => 'span4', 'placeholder' => '.span4')))
-        ->add('text6', 'text', array('attr' => array('class' => 'span5', 'placeholder' => '.span5')))
-        ->add('text8', 'text', array('disabled' => true, 'attr' => array('placeholder' => 'disabled field')))
-        ->add('textarea', 'textarea')
-        ->add('email', 'email')
-        ->add('integer', 'integer')
-        ->add('money', 'money')
-        ->add('number', 'number')
-        ->add('password', 'password')
-        ->add('percent', 'percent')
-        ->add('search', 'search')
-        ->add('url', 'url')
-        ->add('choice1', 'choice',  array(
-            'choices'  => $choices,
-            'multiple' => true,
-            'expanded' => true
-        ))
-        ->add('choice2', 'choice',  array(
-            'choices'  => $choices,
-            'multiple' => false,
-            'expanded' => true
-        ))
-        ->add('choice3', 'choice',  array(
-            'choices'  => $choices,
-            'multiple' => true,
-            'expanded' => false
-        ))
-        ->add('choice4', 'choice',  array(
-            'choices'  => $choices,
-            'multiple' => false,
-            'expanded' => false
-        ))
-        ->add('country', 'country')
-        ->add('language', 'language')
-        ->add('locale', 'locale')
-        ->add('timezone', 'timezone')
-        ->add('date', 'date')
-        ->add('datetime', 'datetime')
-        ->add('time', 'time')
-        ->add('birthday', 'birthday')
-        ->add('checkbox', 'checkbox')
-        ->add('file', 'file')
-        ->add('radio', 'radio')
-        ->add('password_repeated', 'repeated', array(
-            'type'            => 'password',
-            'invalid_message' => 'The password fields must match.',
-            'options'         => array('required' => true),
-            'first_options'   => array('label' => 'Password'),
-            'second_options'  => array('label' => 'Repeat Password'),
-        ))
-        ->getForm()
-    ;
-
-    if ($request->isMethod('POST')) {
-        $form->bind($request);
-        if ($form->isValid()) {
-            $app['session']->getFlashBag()->add('success', 'The form is valid');
-        } else {
-            $form->addError(new FormError('This is a global error'));
-            $app['session']->getFlashBag()->add('info', 'The form is bind, but not valid');
-        }
-    }
-
-    return $app['twig']->render('form.html.twig', array('form' => $form->createView()));
-})->bind('form');
-
 $app->match('/logout', function() use ($app) {
     $app['session']->clear();
 
     return $app->redirect($app['url_generator']->generate('homepage'));
 })->bind('logout');
 
-$app->get('/page-with-cache', function() use ($app) {
-    $response = new Response($app['twig']->render('page-with-cache.html.twig', array('date' => date('Y-M-d h:i:s'))));
-    $response->setTtl(10);
+$app->match('/repo', function(Request $request) use ($app) {
+    $form = $app['form.factory']->createBuilder('form')
+        ->add('github_username', 'text', array('label' => 'Github Username', 'data' => $app['session']->get('_security.last_username')))
+        ->add('github_password', 'password', array('label' => 'Github Password'))
+        ->getForm()
+    ;
 
-    return $response;
-})->bind('page_with_cache');
+    if ('POST' == $request->getMethod()) {
+        $form->bind($request);
 
+        if ($form->isValid()) {
+            $data = $form->getData();
+            $user = $data['github_username'];
+            $repos = $app['github']->api('user')->repositories($user);
+            return $app['twig']->render('repos.html.twig', array(
+                'data' => $repos,
+            ));
+        }
+    }
+    
+    return $app['twig']->render('repo.html.twig', array(
+        'form' => $form->createView(),
+        'error' => $app['security.last_error']($request),
+    ));
+})->bind('repo');
 
 $app->error(function (\Exception $e, $code) use ($app) {
     if ($app['debug']) {
